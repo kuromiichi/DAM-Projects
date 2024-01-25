b9074cba5531e8970a82c29ed1b8224a873c5aed
kuromiichi <emmalikescoding@gmail.com>
2024-01-25 02:15:59 +0100

    juego de 7 y medio

--- PSP/Juego7yMedio/src/dev/kuromiichi/juego7ymedio/Client.java
+++ PSP/Juego7yMedio/src/dev/kuromiichi/juego7ymedio/Client.java
@@ -1,10 +1,11 @@
-package dev.kuromiichi.juego7ymedia;
+package dev.kuromiichi.juego7ymedio;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.PortUnreachableException;
+import java.util.Scanner;
 
 public class Client {
     private static final int MAX_SIZE = 1024;
@@ -19,6 +20,7 @@ public class Client {
 
         try (DatagramSocket socket = new DatagramSocket()) {
             joinGame(socket, playerName);
+            play(socket, playerName);
         } catch (PortUnreachableException e) {
             System.out.println("No se ha podido conectar con el servidor");
         } catch (IOException e) {
@@ -26,10 +28,32 @@ public class Client {
         }
     }
 
+    private static void play(DatagramSocket socket, String playerName) throws IOException {
+        while (true) {
+            DatagramPacket receivedPacket = receivePacket(socket);
+            String message = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+            if (message.equals("continue")) {
+                Scanner scanner = new Scanner(System.in);
+                String answer;
+                do {
+                    answer = scanner.nextLine();
+                    if (!answer.equals("s") && !answer.equals("n")) {
+                        System.out.println("Respuesta inválida");
+                    }
+                } while (!answer.equals("s") && !answer.equals("n"));
+                answer += " " + playerName;
+                sendPacket(socket, InetAddress.getByName(HOST), PORT, answer.getBytes());
+            } else if (message.equals("endround")) {
+                break;
+            } else {
+                System.out.println(message);
+            }
+        }
+    }
+
     private static void joinGame(DatagramSocket socket, String playerName) throws IOException {
-        InetAddress serverAddress = InetAddress.getByName(HOST);
-        socket.connect(serverAddress, PORT);
-        sendPacket(socket, serverAddress, PORT,
+        socket.connect(InetAddress.getByName(HOST), PORT);
+        sendPacket(socket, InetAddress.getByName(HOST), PORT,
                 ("player " + playerName).getBytes());
 
         DatagramPacket receivedPacket = receivePacket(socket);
